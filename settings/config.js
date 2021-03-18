6105ec9659ebad0a71633eac855f0f858fd68116
Luffrbot <80769361+Luffrbot@users.noreply.github.com>
2021-03-18 17:08:38 -0300

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,6 @@
+const config = {
+        botName: 'KRATOS_BOT',
+        ownerName: 'kratos',
+        youtube: 'https://youtube.com/channel/UC1lsMlGC6OI_llwN0xeEXZA',
+        instagram: 'https://instagram.com/animexx_png?igshid=e7zziiwn3d9y',
+}
