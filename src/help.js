6a79a7a4e44a1c4808bff1ad20eac1c7018f797b
Luffrbot <80769361+Luffrbot@users.noreply.github.com>
2021-03-18 23:16:34 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,170 @@
+const help = (prefix) => {
+➥return `
+      〘 𝙈𝙄𝘿𝙊𝙍𝙄𝙔𝘼 𝘽𝙊𝙏🧸✓ 〙
+
+   ║➥  ✓ [INFORMAÇÕES]
+   ║➥Prefix: 「  ${prefix}  」
+   ║➥Criador : Kratosツ
+   ║➥ Canal do criador: https://youtube.com/channel/UC1lsMlGC6OI_llwN0xeEXZA
+   ║➥ Wpp :http://wa.me/+5586994292223*
+   ║
+   ║➣〘 𝙈𝙀𝙉𝙐 𝙂𝙀𝙍𝘼𝙇 〙
+   ║
+   ║➥〘𝗦𝗢𝗕𝗥𝗘 〙
+   ║
+   ║➥${prefix}info)
+   ║➥${prefix}blocklist
+   ║➥${prefix}chatlist
+   ║➥${prefix}ping
+   ║➥${prefix}dono
+   ║➥${prefix}appbot
+   ║➥${prefix}bugreport
+   ║
+   ║➣𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝘿𝙀 𝙁𝙄𝙂𝙐𝙍𝙄𝙉𝙃𝘼🧃 
+   ║        
+   ║➥ 🖥 ${prefix}sticker
+   ║➥ 🖥 ${prefix}stickergif
+   ║➥ 🖥 ${prefix}toimg
+   ║
+   ║➣ 𝙇𝙊𝙂𝙊𝙎🍂
+   ║
+   ║➥ 🖥 ${prefix}tomp3
+   ║➥ 🖥 ${prefix}bpink
+   ║➥ 🖥 ${prefix}marvellogo
+   ║➥ 🖥 ${prefix}snowwrite
+   ║➥ 🖥 ${prefix}3dtext
+   ║➥ 🖥 ${prefix}ninjalogo
+   ║➥ 🖥 ${prefix}water
+   ║➥ 🖥 ${prefix}firetext
+   ║➥ 🖥 ${prefix}logowolf
+   ║➥ 🖥 ${prefix}logowolf2                                                                       
+   ║➥ 🖥 ${prefix}phlogo
+   ║➥ 🖥 ${prefix}glitch
+   ║➥ 🖥 ${prefix}neonlogo
+   ║➥ 🖥 ${prefix}neonlogo2
+   ║➥ 🖥 ${prefix}lionlogo
+   ║➥ 🖥 ${prefix}jokerlogo
+   ║➥ 🖥 ${prefix}shadow
+   ║➥ 🖥 ${prefix}burnpaper
+   ║➥ 🖥 ${prefix}coffee
+   ║➥ 🖥 ${prefix}lovepaper
+   ║➥ 🖥 ${prefix}woodblock
+   ║➥ 🖥 ${prefix}qowheart
+   ║➥ 🖥 ${prefix}mutgrass
+   ║➥ 🖥 ${prefix}undergocean
+   ║➥ 🖥 ${prefix}woodenboards
+   ║➥ 🖥 ${prefix}wolfmetal
+   ║➥ 🖥 ${prefix}metalictglow
+   ║➥ 🖥 ${prefix}8bit
+   ║➥ 🖥 ${prefix}ttp
+   ║➥ 🖥 ${prefix}herrypotter
+   ║➥ 🖥 ${prefix}pubglogo
+   ║➥ 🖥${prefix}quotemaker
+   ║
+   ║➣ 𝙈𝙀𝘿𝙄𝘼🧸
+   ║ 
+   ║➥ 🖥 ${prefix}trendtwit
+   ║➥ 🖥 ${prefix}randomkpop
+   ║➥🖥 ${prefix}ytsearch
+   ║
+   ║➣𝙋𝙀𝙎𝙌𝙐𝙄𝙎𝘼𝙎🔍
+   ║
+   ║➥ 🖥 ${prefix}wiki
+   ║➥ 🖥 ${prefix}wikien
+   ║➥ 🖥 ${prefix}nulis
+   ║➥ 🖥 ${prefix}quotes
+   ║➥ 🖥 ${prefix}quotes2
+   ║➥ 🖥 ${prefix}artinama
+   ║
+   ║➣𝙆𝘼𝙋𝘼𝙉𝙆𝘼𝙃🥯
+   ║➥ 🖥 ${prefix}apakah
+   ║➥ 🖥 ${prefix}kapankah
+   ║➥ 🖥 ${prefix}rate
+   ║➥  🖥 ${prefix}bisakah
+   ║
+   ║➣𝘿𝙊𝙒𝙉𝙇𝙊𝘼𝘿🍬
+   ║
+   ║➥ 🖥 ${prefix}images
+   ║➥ 🖥 ${prefix}ytmp3
+   ║➥ 🖥 ${prefix}ytmp4
+   ║➥ 🖥 ${prefix}tiktok
+   ║➥🖥 ${prefix}joox
+   ║
+   ║➣𝙈𝙀𝙈𝙀🍷
+   ║
+   ║➥${prefix}meme
+   ║➥${prefix}memeindo
+   ║ 
+   ║➣𝙋𝙍𝙊𝘾𝙐𝙍𝘼 𝙈𝙐𝙎𝙄𝘾𝘼𝙎☔
+   ║
+   ║➥ 🖥 ${prefix}play
+   ║➥ 🖥 ${prefix}tts
+   ║
+   ║➣𝙄𝙎𝙇𝘼𝙈🍉
+   ║
+   ║➥ 🖥 ${prefix}lirik
+   ║➥ 🖥 ${prefix}chord
+   ║➥ 🖥 ${prefix}quran 
+   ║
+   ║➣𝙎𝙏𝘼𝙇𝙆🔍
+   ║
+   ║➥🖥 ${prefix}tiktokstalk
+   ║➥ 🖥 ${prefix}igstalk
+   ║
+   ║➣𝙒𝙄𝘽𝙐🧸
+   ║
+   ║➥ 🖥 ${prefix}neonime𝙯
+   ║➥ 🖥 ${prefix}pokemon
+   ║➥ 🖥 ${prefix}loli
+   ║➥ 🖥 ${prefix}waifu
+   ║➥ 🖥 ${prefix}randomanime
+   ║➥ 🖥 ${prefix}husbu
+   ║➥ 🖥 ${prefix}husbu2
+   ║➥ 🖥 ${prefix}wait
+   ║➥ 🖥 ${prefix}nekonime
+   ║
+   ║➣𝙄𝙉𝙏𝙀𝙍𝘼ÇÃO🧸
+   ║
+   ║➥ 🖥 ${prefix}alay
+   ║➥ 🖥 ${prefix}gantengcek
+   ║➥ 🖥 ${prefix}watak
+   ║➥ 🖥 ${prefix}hobby
+   ║➥ 🖥 ${prefix}game
+   ║➥ 🖥 ${prefix}bucin
+   ║➥ 🖥 ${prefix}trust
+   ║➥ 🖥 ${prefix}dare
+   ║➥🖥 ${prefix}simi
+   ║
+   ║➣INFORMAÇÕES DO DIA DIA🐊
+   ║➥ 🖥 ${prefix}bahasa
+   ║➥ 🖥 ${prefix}kodenegara
+   ║➥ 🖥 ${prefix}kbbi
+   ║➥ 🖥 ${prefix}fakta
+   ║➥ 🖥 ${prefix}infocuaca
+   ║➥ 🖥 ${prefix}infogempa
+   ║➥ 🖥 ${prefix}jadwaltvnow
+   ║➥ 🖥 ${prefix}covid
+   ║
+   ║➣𝘾𝙊𝙈𝘼𝙉𝘿𝙊𝙎 𝘿𝙊 𝙋𝙍𝙊𝙋𝙍𝙄𝙀𝙏𝘼𝙍𝙄𝙊☔
+   ║
+   ║➥ 🖥 ${prefix}setprefix
+   ║➥ 🖥 ${prefix}block
+   ║➥ 🖥 ${prefix}bc
+   ║➥ 🖥 ${prefix}bcgc
+   ║➥ 🖥 ${prefix}clone
+   ║➥ 🖥 ${prefix}clearall
+   ║ 
+   ║➣ 𝙊𝙐𝙏𝙍𝙊𝙎🧢
+   ║➥ 🖥 ${prefix}send
+   ║➥ 🖥 ${prefix}wame
+   ║➥ 🖥 ${prefix}virtex
+   ║➥ 🖥 ${prefix}exe
+   ║➥ 🖥 ${prefix}qrcode
+   ║➥ 🖥 ${prefix}afk
+   ║➥ 🖥 ${prefix}timer
+   ║➥ 🖥 ${prefix}fml
+   ║➥🖥 ${prefix}fml2
+`
+}
+
+exports.help = help
